c676d37fd3d3eccbe7244b767f8b32a40b1fdcab
Peter Kreppein <pmkreppein@gmail.com>
2018-10-11 21:42:06 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,9 +14,18 @@ class Author
   def add_post(post)
     post.author = this
   end
-  
- def self.post_count
+ 
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def self.post_count
     Post.all.count
   end
-  
 end #CLSEND
